37b3e297d3f72fd8898aebc462e327e6a5775aea
Ryan Thomas <rmthoma@deakin.edu.au>
2017-01-11 02:56:34 +1100

    Added batsman dismissals and new batsmen.  Added more comprehensive team stats
    Team stats stored in teamStatus Array of name, score, batting
    eligibility and not out status.

--- Game.js
+++ Game.js
@@ -3,6 +3,14 @@ var diceX, diceY, result, totalScore;
 var balls = 0, ballsRemaining = 6, ballsThatOver = 0, dot = 0, wickets = 0, lbw = 0, caught = 0, totalWickets = 0, 
 	run1 = 0, run2 = 0, run3 = 0, run4 = 0, run6 = 0, nb = 0, wd = 0, partnership = 0, partnershipCounter = 0;
 var partnerships = [];
+var batter1 = 0, batter2 = 1;
+var team1 = team_Australia;
+
+		//Displayed at the start of the game showing the teams and initial batsmen
+		function FirstMessages() {
+			console.log("The match is between " + team1.teamName + " and another team");
+			console.log("The first batsmen out are " + team1.teamStatus[batter1][0] + " and " + team1.teamStatus[batter2][0]);
+		}
 
 		//Rolls the dice
 		function RollDice() {
@@ -11,6 +19,47 @@ var partnerships = [];
 			diceY = Math.floor((Math.random() * 12));
 			console.log(diceX+1, diceY+1);
 			balls++;
+		}
+
+		function EndOfPartnership() {
+			var randOut = Math.floor((Math.random() * 2));
+			
+			//If batsman 1 is dismissed
+			if (randOut == 0) {
+				console.log("Batter 1 " + team1.teamStatus[batter1][0] + " is out!");
+				//Check dismissed batsmen and current batsman and bring in next batsman				
+				team1.teamStatus[batter1][2] = false;
+				team1.teamStatus[batter1][1] = team1.teamStatus[batter1][1] + 1;
+				team1.teamStatus[batter1][3] = false;
+
+				for (var i = 0; i < 11; i++) {
+					if (team1.teamStatus[i][2] == true) {
+						batter1 = i;
+						team1.teamStatus[i][2] = false;
+						break;
+					}
+				}
+
+				console.log("The new batter is " + team1.teamStatus[batter1][0]);
+			} 
+			//If batsman 2 is dismissed
+			else if (randOut == 1) {
+				console.log("Batter 2 " + team1.teamStatus[batter2][0] + " is out!");
+				//Check dismissed batsmen and current batsman and bring in next batsman
+				team1.teamStatus[batter2][2] = false;
+				team1.teamStatus[batter2][1] = team1.teamStatus[batter2][1] + 1;
+				team1.teamStatus[batter2][3] = false;
+
+
+				for (var i = 0; i < 11; i++) {
+					if (team1.teamStatus[i][2] == true) {
+						batter2 = i;
+						team1.teamStatus[i][2] = false;
+						break;
+					}
+				}
+				console.log("The new batter is " + team1.teamStatus[batter2][0]);
+			}
 		}
 
 		//Each ball it calculates the result from the table. It also adds an extra ball for wides and no balls.
@@ -29,11 +78,12 @@ var partnerships = [];
 			}
 
 			if (resultTable[diceX][diceY] == "W" || resultTable[diceX][diceY] == "LBW" || resultTable[diceX][diceY] == "C") {
-				console.log("OUT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-				console.log(partnership);
+				//console.log("OUT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+				//console.log(partnership);
 				partnerships[partnershipCounter] = partnership;
 				partnership = 0
 				partnershipCounter++;
+				EndOfPartnership();
 			}
 
 			if (ballsRemaining > 1) {
@@ -51,7 +101,7 @@ var partnerships = [];
 		//THE MAIN FUNCTION.
 		//Calls RollDice and EachBall methods and adds the result to their respective variables (runs and outs).
 		function TestBalls() {
-			while (totalWickets < 11) {
+			while (totalWickets < 10) {
 				RollDice();
 				EachBall();
 				switch(result) {
@@ -112,8 +162,11 @@ var partnerships = [];
 		//RollDice();
 		//EachBall();
 		
+		FirstMessages();
+		console.log('\n');
 		TestBalls();
 		CalculateScore();
+		console.log(team1.team)
 
 		console.log('\n');
 		console.log("Total Balls: " + balls)
@@ -131,5 +184,5 @@ var partnerships = [];
 		console.log("Total Score: " + totalScore);
 		console.log('\n');
 
-		for (var i = 0; i < 11; i++)
+		for (var i = 0; i < 10; i++)
 			console.log(partnerships[i]);
