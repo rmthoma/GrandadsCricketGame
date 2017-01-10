7d0ea3d656c8e63b97183f8e746256782df01b4d
Ryan Thomas <rmthoma@deakin.edu.au>
2017-01-10 20:50:08 +1100

    Added partnership tracking
    Adds partnership total to partnerships array after each out. Also added
    better commenting.

--- Game.js
+++ Game.js
@@ -1,6 +1,10 @@
+//Declares all global variables
 var diceX, diceY, result, totalScore;
-var balls = 0, ballsRemaining = 6, ballsThatOver = 0, dot = 0, wickets = 0, lbw = 0, caught = 0, totalWickets = 0, run1 = 0, run2 = 0, run3 = 0, run4 = 0, run6 = 0, nb = 0, wd = 0;
+var balls = 0, ballsRemaining = 6, ballsThatOver = 0, dot = 0, wickets = 0, lbw = 0, caught = 0, totalWickets = 0, 
+	run1 = 0, run2 = 0, run3 = 0, run4 = 0, run6 = 0, nb = 0, wd = 0, partnership = 0, partnershipCounter = 0;
+var partnerships = [];
 
+		//Rolls the dice
 		function RollDice() {
 			console.log(ballsRemaining + " balls remaining");
 			diceX = Math.floor((Math.random() * 12));
@@ -9,6 +13,9 @@ var balls = 0, ballsRemaining = 6, ballsThatOver = 0, dot = 0, wickets = 0, lbw 
 			balls++;
 		}
 
+		//Each ball it calculates the result from the table. It also adds an extra ball for wides and no balls.
+		//It also keeps track of how many balls have been bowled (total balls that over and balls remaining).
+		//It also keeps track of partnerships and stores each partnership total in partnerships array after out.
 		function EachBall() {
 			if (resultTable[diceX][diceY] != null) {
 				console.log(resultTable[diceX][diceY]);
@@ -19,6 +26,14 @@ var balls = 0, ballsRemaining = 6, ballsThatOver = 0, dot = 0, wickets = 0, lbw 
 
 			if (resultTable[diceX][diceY] == "NB" || resultTable[diceX][diceY] == "WD") {
 				ballsRemaining++;
+			}
+
+			if (resultTable[diceX][diceY] == "W" || resultTable[diceX][diceY] == "LBW" || resultTable[diceX][diceY] == "C") {
+				console.log("OUT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+				console.log(partnership);
+				partnerships[partnershipCounter] = partnership;
+				partnership = 0
+				partnershipCounter++;
 			}
 
 			if (ballsRemaining > 1) {
@@ -33,7 +48,8 @@ var balls = 0, ballsRemaining = 6, ballsThatOver = 0, dot = 0, wickets = 0, lbw 
 			}
 		}
 
-
+		//THE MAIN FUNCTION.
+		//Calls RollDice and EachBall methods and adds the result to their respective variables (runs and outs).
 		function TestBalls() {
 			while (totalWickets < 11) {
 				RollDice();
@@ -56,31 +72,39 @@ var balls = 0, ballsRemaining = 6, ballsThatOver = 0, dot = 0, wickets = 0, lbw 
 				        break;
 				    case "1":
 				        run1++;
+				        partnership = partnership + 1;
 				        break;
 				    case "2":
 				        run2++;
+				        partnership = partnership + 2;
 				        break;
 				    case "3":
 				        run3++;
+				        partnership = partnership + 3;
 				        break;
 				    case "4":
 				        run4++;
+				        partnership = partnership + 4;
 				        break;
 				    case "6":
 				        run6++;
+				        partnership = partnership + 6;
 				        break;
 				    case "NB":
 				        nb++;
+				        partnership = partnership + 1;
 				        break;
 				    case "WD":
 				        wd++;
+				        partnership = partnership + 1;
 				        break;
 				    default:
-				        console.log("default case");
+				        console.log("default case. THIS SHOULDN'T HAPPEN!");
 				}
 			}
 		}
 
+		//Calculates the total score of all runs, wides, no balls etc.
 		function CalculateScore() {
 			totalScore = (1 * run1) + (2 * run2) + (3 * run3) + (4 * run4) + (6 * run6) + (1 * nb) + (1 * wd);
 		}
@@ -105,3 +129,7 @@ var balls = 0, ballsRemaining = 6, ballsThatOver = 0, dot = 0, wickets = 0, lbw 
 		console.log("Wides: " + wd);
 		console.log("No Balls: " + nb);
 		console.log("Total Score: " + totalScore);
+		console.log('\n');
+
+		for (var i = 0; i < 11; i++)
+			console.log(partnerships[i]);
---
+++ Team_Australia.js
@@ -0,0 +1,15 @@
+/*
+D. Warner
+A. Finch
+S. Smith
+G. Bailey
+M. Marsh
+T. Head
+S. Wade
+J. Faulkner
+M. Starc
+P. Cummins
+J. Hazlewood
+*/
+
+australiaPlayers = ["D. Warner", "A. Finch", "S. Smith", "G. Bailey", "M. Marsh", "T. Head", "S. Wade", "J. Faulkner", "M. Starc", "P. Cummins", "J. Hazlewood"];
